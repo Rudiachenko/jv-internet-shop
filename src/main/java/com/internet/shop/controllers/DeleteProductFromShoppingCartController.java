76b8d0761dfabb55a206dbd58b3461bf440b7207
Fedia <fedia.rudiachenko@gmail.com>
2020-09-10 00:44:25 +0300

    added solution

--- src/main/java/com/internet/shop/controllers/DeleteProductFromShoppingCartController.java
+++ src/main/java/com/internet/shop/controllers/DeleteProductFromShoppingCartController.java
@@ -6,7 +6,6 @@ import com.internet.shop.model.ShoppingCart;
 import com.internet.shop.service.ProductService;
 import com.internet.shop.service.ShoppingCartService;
 import java.io.IOException;
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
