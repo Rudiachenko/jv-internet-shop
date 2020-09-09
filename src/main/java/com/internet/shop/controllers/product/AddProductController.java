f2b6fbfda3be61e2ce9dc254dc7cf598da4063cc
Fedia <fedia.rudiachenko@gmail.com>
2020-09-10 00:53:51 +0300

    added solution

--- src/main/java/com/internet/shop/controllers/product/AddProductController.java
+++ src/main/java/com/internet/shop/controllers/product/AddProductController.java
@@ -1,16 +1,15 @@
-package com.internet.shop.controllers;
+package com.internet.shop.controllers.product;
 
 import com.internet.shop.lib.Injector;
 import com.internet.shop.model.Product;
 import com.internet.shop.service.ProductService;
 import java.io.IOException;
-import java.util.List;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class ProductController extends HttpServlet {
+public class AddProductController extends HttpServlet {
     private static final Injector injector = Injector.getInstance("com.internet.shop");
     private final ProductService productService =
             (ProductService) injector.getInstance(ProductService.class);
@@ -18,8 +17,16 @@ public class ProductController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
-        List<Product> allProducts = productService.getAll();
-        req.setAttribute("products", allProducts);
-        req.getRequestDispatcher("/WEB-INF/views/product/all.jsp").forward(req, resp);
+        req.getRequestDispatcher("/WEB-INF/views/product/add.jsp").forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+            throws IOException {
+        String product = req.getParameter("product");
+        String price = req.getParameter("price");
+        Product item = new Product(product, Double.parseDouble(price));
+        productService.create(item);
+        resp.sendRedirect(req.getContextPath() + "/products/all");
     }
 }
